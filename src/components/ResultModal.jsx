9c3160c54c9ddbb7fa607e57c5db8a75312fe2be
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-19 15:07:40 +0100

    closing modal with Esc

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -17,7 +17,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     });
 
     return (
-        <dialog className="result-modal" ref={dialog}>
+        <dialog className="result-modal" ref={dialog} onClose={onReset}>
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your score : {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
